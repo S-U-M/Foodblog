fb751a80a7bd5ff9903f3eb1b148728e2172c771
S-U-M <suyeluri@gmail.com>
2026-03-22 00:16:58 -0700

    Fixed UI

--- src/View/Home.js
+++ src/View/Home.js
@@ -1,9 +1,35 @@
+import "../Style/Style.css";
+
 function Home() {
     return (
-        <div>
-            <h1>FoodBlog</h1>
-            <p>Show your food to the world</p>
+        <div className="container"> 
+
+        {/* Navbar */}
+        <div className="navbar">
+             <h1 className="logo">FoodBlog</h1>
+        
+            <div className="nav-buttons">
+                <button className="upload-btn">Upload</button>
+                <button className="about-btn">AboutUs</button>
+            </div>
         </div>
+
+        {/* Main Content: */}
+        <div className="content">
+            <h2>Show your food to the world</h2>
+
+            <div className="upload-section">
+                <div className="upload-placeholder">+</div>
+                <div className="upload-placeholder">+</div>
+                <div className="upload-placeholder">+</div>
+            </div>
+
+            <div className="cancel-btn">
+                <p>Cancel all uploads</p>
+            </div>
+        </div>
+
+    </div>
     );
 }
 
